b665a3c30d3db543d2fb83379a71eb28c44b2999
rbd3 <rabedesanaa@gmail.com>
2023-09-21 17:23:06 +0300

    Fix comment issue

--- db/migrate/20230921090105_create_comments.rb
+++ db/migrate/20230921090105_create_comments.rb
@@ -1,21 +1,17 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :author, foreign_key: { to_table: :users }
-      t.references :post, foreign_key: { to_table: :posts }
+      t.bigint :author_id
+      t.bigint :post_id
       t.text :text
 
       t.timestamps
     end
 
-    # indexing foreign_key column "author_id"
-    unless index_exists?(:comments, :author_id)
-      add_index :comments, :author_id
-    end
+    add_foreign_key :comments, :users, column: :author_id
+    add_foreign_key :comments, :posts, column: :post_id
 
-    # indexing foreign_key column "author_id"
-    unless index_exists?(:comments, :post_id)
-      add_index :comments, :post_id
-    end
+    add_index :comments, :author_id
+    add_index :comments, :post_id
   end
 end
