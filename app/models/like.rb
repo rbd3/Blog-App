c2ab2b91a1956fb1662c688a9eab821f542b9c19
rbd3 <rabedesanaa@gmail.com>
2023-09-22 08:27:35 +0300

    Implement reviwer feedback

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_create :increment_like_posts_counter
+
+  def increment_like_posts_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  after_create :increment_user_posts_counter
+
+  def increment_user_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
