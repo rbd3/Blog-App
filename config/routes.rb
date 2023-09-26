a4476e1345afc3b565d189c90129acce0ee50ad6
rbd3 <rabedesanaandry01@gmail.com>
2023-09-26 21:50:42 +0300

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-  get 'posts/show'
   
-  get '/users', to: 'users#index', as: 'users'
-  get '/users/:id', to: 'users#show', as: 'user'
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  #resources :users
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
