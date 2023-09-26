a2df8c7d06d3f2fd3250e25cace0ee8303fe0409
rbd3 <rabedesanaandry01@gmail.com>
2023-09-26 20:53:53 +0300

    Add posts controler

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  #get 'user/index'
-  #get 'user/show'
+  get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  get 'posts/show'
+  
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/:id', to: 'users#show', as: 'user'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users
+  #resources :users
 end
