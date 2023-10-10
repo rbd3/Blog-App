6c56531b8074d4d581d0bcea430210ccf52133d3
rbd3 <rabedesanaandry01@gmail.com>
2023-10-10 11:31:14 +0300

    Fix linters error

--- spec/views/user/index.html.erb_spec.rb
+++ spec/views/user/index.html.erb_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.feature "User Index Page", type: :feature do
+RSpec.feature 'User Index Page', type: :feature do
   let!(:user1) { User.create(name: 'user1', photo: 'user1.jpg') }
   let!(:user2) { User.create(name: 'user2', photo: 'user2.jpg') }
   let!(:post1) { Post.create(author: user1) }
   let!(:post2) { Post.create(author: user1) }
 
-  scenario "Displaying User Information on the Index Page" do
+  scenario 'Displaying User Information on the Index Page' do
     visit users_path
 
     expect(page).to have_content(user1.name)
@@ -16,10 +16,10 @@ RSpec.feature "User Index Page", type: :feature do
     expect(page).to have_css("img[src*='user2.jpg']")
 
     expect(page).to have_content("Number of posts: #{user1.posts.count}")
-    expect(page).to have_content("Number of posts: 0") # User2 has no posts
+    expect(page).to have_content('Number of posts: 0') # User2 has no posts
   end
 
-  scenario "Clicking on a User Redirects to Their Show Page" do
+  scenario 'Clicking on a User Redirects to Their Show Page' do
     visit users_path
 
     click_link user1.name
